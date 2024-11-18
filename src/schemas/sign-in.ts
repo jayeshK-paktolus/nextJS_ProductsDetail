346628ce334066711a6e9aafcbd6e5a8d68c26eb
“gaburto-paktolus” <gaburto@paktolus.com>
2024-11-18 13:09:46 -0600

    Remove password strength validation

--- src/schemas/sign-in.ts
+++ src/schemas/sign-in.ts
@@ -2,13 +2,5 @@ import z from "zod";
 
 export const SingInFormSchema = z.object({
   email: z.string().email({ message: "Please enter a valid email." }).trim(),
-  password: z
-    .string()
-    .min(8, { message: "Be at least 8 characters long" })
-    .regex(/[a-zA-Z]/, { message: "Contain at least one letter." })
-    .regex(/[0-9]/, { message: "Contain at least one number." })
-    .regex(/[^a-zA-Z0-9]/, {
-      message: "Contain at least one special character.",
-    })
-    .trim(),
+  password: z.string().min(1, { message: "Password is required" }).trim(),
 });
