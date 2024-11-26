064a215f705a31ee1fd72c07d954d2a0830bc458
“gaburto-paktolus” <gaburto@paktolus.com>
2024-11-26 11:11:11 -0600

    Add tests using jest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import { HelloClient } from "@/components/hello-client";
-import { HelloServer } from "@/components/hello-server";
-
 export default function Home() {
   return (
     <div className="grid place-content-center">
-      <HelloServer />
-      <HelloClient />
+      <h1>Home</h1>
     </div>
   );
 }
