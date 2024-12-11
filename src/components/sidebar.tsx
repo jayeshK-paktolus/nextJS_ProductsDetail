d4f2e6191327e3863fac459d0f04ccb345fe0229
Aniket-Karne-Git <ankarne@paktolus.com>
2024-12-11 16:26:07 +0530

    Added hover effect on sidebar link list

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -18,21 +18,21 @@ const Sidebar = ({ isOpen }: { isOpen: boolean }) => {
       <aside>
         <div className="h-full pt-4 pb-4 m-[20px]">
           <ul>
-            <li className="hover:bg-gray-100 rounded">
+            <li className="hover:bg-gray-100 rounded ">
               <Link
-                className="flex items-center p-2 text-gray-800 rounded-lg mb-[-10px] font-bold"
+                className="flex items-center p-2 text-gray-800 rounded-lg mb-[-10px] font-bold hover:text-gray-950"
                 href="/"
               >
-                <ChartPie className="text-gray-500 transition-colors duration-[75ms] m-2" />
+                <ChartPie className="text-gray-500 transition-colors duration-[75ms] m-2 hover:text-gray-950 hover:text-gray-950 " />
                 Dashboard
               </Link>
             </li>
             <li className="hover:bg-gray-100 rounded">
               <Link
-                className="flex items-center p-2 text-gray-800 rounded-lg mb-[-10px] font-bold"
+                className="flex items-center p-2 text-gray-800 rounded-lg mb-[-10px] font-bold hover:text-gray-950"
                 href="/products"
               >
-                <SquareChartGantt className="text-gray-500 transition-colors duration-[75ms] m-2" />
+                <SquareChartGantt className="text-gray-500 transition-colors duration-[75ms] m-2 hover:text-gray-950" />
                 Products
               </Link>
             </li>
