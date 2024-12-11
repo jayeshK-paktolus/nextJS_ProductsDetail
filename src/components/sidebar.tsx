64ce832313fe2eb182495c149ecfcd14bd90f751
Aniket-Karne-Git <ankarne@paktolus.com>
2024-12-11 16:28:22 +0530

    removed the es lint warnings

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -23,7 +23,7 @@ const Sidebar = ({ isOpen }: { isOpen: boolean }) => {
                 className="flex items-center p-2 text-gray-800 rounded-lg mb-[-10px] font-bold hover:text-gray-950"
                 href="/"
               >
-                <ChartPie className="text-gray-500 transition-colors duration-[75ms] m-2 hover:text-gray-950 hover:text-gray-950 " />
+                <ChartPie className="text-gray-500 transition-colors duration-[75ms] m-2 hover:text-gray-950 " />
                 Dashboard
               </Link>
             </li>
