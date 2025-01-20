c4b7bc3a254da1da21412c0d020d5f20a0159899
Aniket-Karne-Git <ankarne@paktolus.com>
2025-01-20 13:28:24 +0530

    removed vite logo and added nextjs logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -32,7 +32,7 @@ const Navbar = ({
 
           <Link href="/" className="mr-8 flex">
             <Image
-              src="/vite.svg"
+              src="/nexjs.svg"
               alt="Dashboard Logo"
               className="mr-3"
               width={32}
