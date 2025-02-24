867905bc0ca61df6b32778d31621c0bafbbf8476
“gaburto-paktolus” <gaburto@paktolus.com>
2025-02-24 13:31:57 -0600

    Split next auth configuration

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,6 +1,6 @@
 import { cache } from "react";
 
-import { auth } from "@/app/api/auth/[...nextauth]/route";
+import { auth } from "@/auth";
 
 export const createTRPCContext = cache(async () => {
   const session = await auth();
